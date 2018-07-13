f40b115d3d1553da9a44373814a6c6d52cd5f57b
Sina Siadat <siadat@gmail.com>
2018-07-14 00:15:03 +0430

    support session names with space in them, fixes #3

--- session-finder.bash
+++ session-finder.bash
@@ -15,7 +15,7 @@ session_status() {
 	counter=0
 	colors=(yellow red green blue magenta cyan white)
 	$tmux ls -F '#{session_attached} #{session_last_attached}#{?session_last_attached,,0} #{session_created} #{session_id} #{session_windows} #{session_name}' \
-		| sort -r | awk '{ print $6 }' | head -n $MAX | while read line; do
+		| sort -r | cut -d' ' -f6- | head -n $MAX | while read line; do
 		local colori=$(expr $counter % ${#colors})
 		# TODO consider width of current window
 		if [ "$counter" = 0 ]; then
@@ -50,31 +50,36 @@ session_next() {
 }
 
 session_finder() {
-	fzf_out=$($tmux ls -F '#{session_attached} #{?session_last_attached,,0}#{session_last_attached} #{session_name}' | grep -v '^1' | sort -r | perl -pe 's/^0 [0-9]+//' | fzf --print-query --prompt="$prompt" || true)
+	fzf_out=$($tmux ls -F '#{session_attached} #{?session_last_attached,,0}#{session_last_attached} #{session_name}' \
+    | grep -v '^1' \
+    | sort -r \
+    | perl -pe 's/^0 [0-9]+ //' \
+    | /home/sina/.fzf/bin/fzf --print-query --prompt="$prompt" \
+    || true)
 	line_count=$(echo "$fzf_out" | wc -l)
-	session_name=$(echo "$fzf_out" | tail -n1)
+	session_name="$(echo "$fzf_out" | tail -n1)"
 	command=$(echo "$session_name" | awk '{ print $1 }')
 
 	if [ $line_count -eq 1 ]; then
 		unset TMUX
 		word_count=$(echo "$fzf_out" | wc -w)
 		if [ $word_count -eq 1 ]; then
-			$tmux new-session -d -s $session_name
-			$tmux switch-client -t $session_name
+			$tmux new-session -d -s "$session_name"
+			$tmux switch-client -t "$session_name"
 		else
-			session_name=$(echo "$fzf_out" | tail -n1 | awk '{ print $2 }')
+			session_name="$(echo "$fzf_out" | tail -n1 | awk '{ print $2 }')"
 			case "$command" in
 				":new")
-					$tmux new-session -d -s $session_name
-					$tmux switch-client -t $session_name
+					$tmux new-session -d -s "$session_name"
+					$tmux switch-client -t "$session_name"
 					;;
 				":rename")
-					$tmux rename-session $session_name
+					$tmux rename-session "$session_name"
 					;;
 			esac
 		fi
 	else
-		$tmux switch-client -t $session_name
+		$tmux switch-client -t "$session_name"
 	fi
 	sleep 0.1
 	$tmux refresh-client -S
